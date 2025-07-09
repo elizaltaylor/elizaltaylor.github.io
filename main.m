236c70869c20763ddf1946d9df2d49c167f79ece
elizaltaylor <eltaylor@hawaii.edu>
2025-07-09 14:04:47 -0400

    update

---
+++ main.m
@@ -0,0 +1,8 @@
+% Check current date
+
+% Look at list of transmission processed, current date and transmsission
+% schedule to see if any were missed
+
+% Make list of transmissions to-do
+
+%Make 
